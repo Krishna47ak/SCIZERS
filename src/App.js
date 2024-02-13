14d0a77976d421ea61f5cc03b37018d952e44725
Krishna47ak <anandakrishna720@gmail.com>
2024-02-13 20:12:20 +0530

    Added functionality to fetch profiles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import store from "./store/store";
 import Home from "./screens/Home";
+import { fetchProfiles } from "./store/actions/profiles";
 
 
 const appRouter = createBrowserRouter([
@@ -10,6 +13,9 @@ const appRouter = createBrowserRouter([
 ])
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(fetchProfiles())
+  }, [])
 
   return (
     <RouterProvider router={appRouter} />
