62558ea844c8896ba1ee19771af910aa2fffee44
Krishna47ak <anandakrishna720@gmail.com>
2024-02-13 20:13:55 +0530

    Created Home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { useSelector } from 'react-redux'
+import NavbarHeader from "../components/Navbar"
+import ProfileCard from "../components/ProfileCard"
+import { Spinner } from "@material-tailwind/react"
+import Pagination from "../components/Pagination"
+
 
 const Home = () => {
+    const { profiles, loading } = useSelector(state => state?.profiles)
+    const [searchText, setSearchtext] = useState("")
+    const [searchData, setSearchData] = useState(profiles)
+
     return (
-        <div>Home</div>
+        <div className='bg-black text-white min-h-screen' >
+            <NavbarHeader value={searchText} onChange={setSearchtext} />
+            {loading ? (
+                <div className="flex h-[85vh] justify-center items-center" >
+                    <Spinner className="h-20 w-20" color="blue" />
+                </div>
+            ) :
+                <div className="p-10 md:p-16">
+                    <div className="grid lg:grid-cols-2 gap-y-7 lg:gap-7" >
+                        {searchData?.map((profile) => (
+                            <ProfileCard key={profile?.name} data={profile} />
+                        ))}
+                    </div>
+                    <div className="flex justify-center mt-10" >
+                        <Pagination />
+                    </div>
+                </div>
+            }
+        </div>
     )
 }
 
